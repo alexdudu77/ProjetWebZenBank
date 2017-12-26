e00c6d991124ce13d6716e9bf97f5f3b894df5e4
alexduvert <34349987+alexdudu77@users.noreply.github.com>
2017-12-26 11:56:49 +0100

    Modification Template
    Modification du template pour prendre en charge l'entête.

--- templateInterfaceClient.php
+++ templateInterfaceClient.php
@@ -11,32 +11,9 @@
 </head>
 <body>
 
-<nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>                        
-      </button>
-      <a class="navbar-brand" href="index.html">Zen Bank</a>
-    </div>
-    <div class="collapse navbar-collapse" id="myNavbar">
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="index.html">Home</a></li>
-        <li class="active"><a href="interfaceClientSyntheseCompte.html">Mon espace client</a></li>
-      </ul>
-      <ul class="nav navbar-nav navbar-right">
-        <li><h2 id="messageBienvenue">Bonjour client</h2></li>
-        <li><a href="index.html"><button type="button" class="btn btn-danger">
-          Se déconnecter <span class="glyphicon glyphicon-log-out"></span>
-        </button></a></li>
-      </ul>
-    </div>
-  </div>
-</nav>
-  
-<div class="container-fluid text-center">    
+<?php include("interfaceClientEnTete.php") ?>
+
+<div class="container-fluid text-center">
   <div class="row content">
     <div class="col-md-3 sidenav">
       <div class="panel-group" id="accordion">
@@ -143,8 +120,8 @@
         </div>
       </div>
     </div>
-    <div class="col-md-9 text-left"> 
-   
+    <div class="col-md-9 text-left">
+
     </div>
   </div>
 </div>
@@ -157,4 +134,3 @@
 
 </body>
 </html>
-
